738f2dcb501a846bb15f08b72635ddc5f44460bc
thalesallan <trabalhecomthales@gmail.com>
2021-05-11 00:02:21 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio2/Pessoa.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicio2
+{
+    class Pessoa
+    {
+        public string Nome;
+        public float Salario;
+    }
+}
---
+++ Exercicio2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa PrimeiroFuncionario, SegundoFuncionario;
+            PrimeiroFuncionario = new Pessoa();
+            SegundoFuncionario = new Pessoa();
+
+            Console.WriteLine("Dados do Primeiro Funcionario:");
+            Console.Write("Nome: ");
+            PrimeiroFuncionario.Nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            PrimeiroFuncionario.Salario = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("Dados do Segundo Funcionario");
+            Console.Write("Nome: ");
+            SegundoFuncionario.Nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            SegundoFuncionario.Salario = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("---------------------------------");
+            float MediaSalarial;
+            MediaSalarial = (PrimeiroFuncionario.Salario + SegundoFuncionario.Salario) / 2;
+            Console.WriteLine("Salário Médio = " + MediaSalarial.ToString("F2", CultureInfo.InvariantCulture));
+
+
+        }
+    }
+}
